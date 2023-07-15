b7eaa5f3cab48d576d3bf0ea2be798aabfb544e2
IT21440236 <it21440236@my.sliit.lk>
2023-07-15 09:06:56 +0530

    commit

---
+++ src/main/java/com/spm/eventmanagementsystem/EventmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.spm.eventmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spm/eventmanagementsystem/EventmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spm.eventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
